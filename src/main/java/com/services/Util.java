881788803294182d1373d82dd25024de893ff38f
Antreas Matrakas <antreaspeis@gmail.com>
2021-02-05 17:58:38 +0200

    -

--- src/main/java/com/services/Util.java
+++ src/main/java/com/services/Util.java
@@ -1,15 +1,28 @@
 package com.services;
 
 import com.dataTransferObjects.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.jcache.JCacheCache;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class Util {
 
-// # sto key gia na parei to pedio
+    @Autowired
+    CacheManager cacheManager;
+
+
+
+    // # sto key gia na parei to pedio
     @Cacheable(cacheNames = "usercache",key = "#user.email")
     public User uniqueId(User user){
         System.out.println("In to the cache- generates the UUID");
@@ -17,4 +30,28 @@ public class Util {
         user.setId(uuid);
         return user;
     }
+
+    public User findUser(String email) {
+
+        for (User i : getUserFromCache()) {
+            System.out.println(i);
+            if (i.getEmail().equals(email)) {
+                return i;
+            }
+        }
+        return null;
+
+    }
+
+
+    public List<User> getUserFromCache() {
+
+        javax.cache.Cache<Object, Object> ehcache = ((JCacheCache) Objects.requireNonNull(cacheManager.getCache("usercache"))).getNativeCache();
+        List<User> coverageMapperList = (List<User>) (Object) StreamSupport.stream(ehcache.spliterator(), false)
+                .collect(Collectors.toList()).stream().map(javax.cache.Cache.Entry::getValue).collect(Collectors.toList());
+
+        return coverageMapperList;
+    }
+
+
 }
